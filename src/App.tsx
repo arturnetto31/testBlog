f1793151eea2c921c2c002985532e05e477b1093
Artur <francisco.artur@dce.ufpb.br>
2023-09-03 22:33:35 -0300

    ajuste routes..

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Router } from "./lib/Router";
+import { DefaultLayout } from "./layouts/DefaultLayout";
+import Home from "./pages/Home";
+import LoginScreen from "./pages/Login";
+import { PostDetail } from "./pages/PostDetail";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router></Router>
+        <Routes>
+          <Route path="/" element={<DefaultLayout></DefaultLayout>}>
+            <Route path="/" element={<LoginScreen></LoginScreen>}></Route>
+            <Route path="/home" element={<Home></Home>}></Route>
+            <Route path="/:id" element={<PostDetail></PostDetail>}></Route>
+          </Route>
+        </Routes>
       </BrowserRouter>
       <GlobalStyle></GlobalStyle>
     </ThemeProvider>
